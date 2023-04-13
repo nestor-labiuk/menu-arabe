66294ebd03bdeb72ef2f0fe0761f196a9613b801
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-13 19:11:11 -0300

    agrega modulo de rutas y le agrega ruta de registro para probar coneccíon con base de datos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Register from './pages/Register/Register'
 
 function App() {
   return (
     <div className="App">
       <h1>Menú Árabe</h1>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
   );
 }
