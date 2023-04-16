226df09b436acd910ad4ead41c236d12225c0c52
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-15 23:38:20 -0300

    agrega ruta administración al app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Register from './pages/Register/Register'
 import Home from './pages/Home/Home'
+import Admin from './pages/Admin/Admin'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={ <Home /> } />
         <Route path='/register' element={<Register />} />
+        <Route path='/admin' element={<Admin />} />
         {/* aca hay que agregar las rutas que faltan */}
       </Routes>
     </>
