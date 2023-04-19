36eec1a88dc0b882df1baefa9deedaf369dd6ab8
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-18 22:25:44 -0300

    Agrega propiedad reset de user form para resetear el formulario al hacer submit

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -4,7 +4,7 @@ import Button from '../../components/Button/Button'
 import { Link } from 'react-router-dom'
 
 const FormRegister = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors }, reset } = useForm()
 
   const createUsers = async (body) => {
     try {
@@ -19,64 +19,56 @@ const FormRegister = () => {
       console.log(error)
     }
   }
-
   const onSubmit = body => {
     createUsers(body)
+    reset()
   }
 
   return (
-    <div className='register-main p-5'>
-
-      <h1 className='text-center p-3 register-title'>Registro</h1>
-
-      <div className=' d-flex justify-content-center'>
-        <form className='d-flex flex-column  register-form' onSubmit={handleSubmit(onSubmit)}>
-          <input
-            placeholder='Nombre'
-            maxLength={30}
-            type='text'{...register('name', { required: true, minLength: 3, maxLength: 12 })}
-          />
-          {errors.name && <span>Longitud mínima es 3 caracteres</span>}
-
-
-          <input
-            placeholder='Email'
-            maxLength={30}
-            type='email' {...register('email', { required: true, })}
-          />
-          {errors.exampleRequired && <span>This field is required</span>}
-
-
-          <input
-            placeholder='Dirección'
-            maxLength={30}
-            type='text' {...register('adress', { required: true, })}
-          />
-          {errors.exampleRequired && <span>This field is required</span>}
-
-
-          <input
-            placeholder='teléfono'
-            maxLength={12}
-            type='textsfd' {...register('phoneNumber', { required: true, })}
-          />
-          {errors.exampleRequired && <span>This field is required</span>}
-
-
-          <input
-            maxLength={12}
-            placeholder='Password'
-            type='text' {...register('password', { required: true })}
-          />
-          {errors.password && <span>This field is required</span>}
-
-          <input type='submit' value='Registrarse' />
-          <input type='reset' value='Reset' />
-        </form>
+    <div>
+      <div className='register-main p-5'>
+        <h1 className='text-center p-3 register-title'>Registro</h1>
+        <div className=' d-flex justify-content-center'>
+          <form className='d-flex flex-column  register-form' onSubmit={handleSubmit(onSubmit)}>
+            <input
+              placeholder='Nombre'
+              maxLength={30}
+              type='text'{...register('name', { required: true, minLength: 3, maxLength: 12 })}
+            />
+            {errors.name && <span>Longitud mínima es 3 caracteres</span>}
+            <input
+              placeholder='Email'
+              maxLength={30}
+              type='email' {...register('email', { required: true, })}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+            <input
+              placeholder='Dirección'
+              maxLength={30}
+              type='text' {...register('adress', { required: true, })}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+            <input
+              placeholder='teléfono'
+              maxLength={12}
+              type='textsfd' {...register('phoneNumber', { required: true, })}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+            <input
+              maxLength={12}
+              placeholder='Password'
+              type='text' {...register('password', { required: true })}
+            />
+            {errors.password && <span>This field is required</span>}
+            <input type='submit' value='Registrarse' />
+            <input type='reset' value='Reset' />
+          </form>
+        </div>
+        <div className='d-flex justify-content-center p-5'>
+          <Link to='/'><Button name='Volver' /></Link>
+        </div>
       </div>
-      <div className='d-flex justify-content-center p-5'>
-        <Link to='/'><Button name='Volver' /></Link>
-      </div>
+      
     </div>
   )
 }
