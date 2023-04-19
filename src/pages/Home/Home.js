442580939a0a18fcc1334d4b1b8e2ad275062494
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-18 22:24:29 -0300

    Agrega foto nueva a home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import './Home.css'
-import baba from '../../assets/img/fondo.jpg'
+import fondo from '../../assets/img/fondo-sin-marca-recortada.jpg'
 import Button from '../../components/Button/Button'
 import { Link } from 'react-router-dom'
 
@@ -8,7 +8,7 @@ function Home() {
     <main className='container-fluid pt-5 px-0 main'>
         <h1 className='text-center pb-3'>Comida Árabe</h1>
       <div className='text-center ' >
-        <img src={baba} className='img-fluid m-0 p-0 ' alt='Imagen de comida árabe' />
+        <img src={fondo} className='img-fluid m-0 p-0 ' alt='Imagen de comida árabe' />
       </div>
       <div className='text-center mx-2'>
         <h2 className='pt-5'>Preparáte para degustar la mejor comida árabe en Tucumán</h2>
