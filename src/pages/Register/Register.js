e29bc23f4db19dc5aa937eae12ad7bc8c6d897ce
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-22 15:10:53 -0300

    agrega mensajes de validacion de toast

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -17,9 +17,22 @@ const FormRegister = () => {
         headers: { 'Content-Type': 'application/json' }
       })
       const data = await response.json()
-      toast('hola')
       console.log(data)
+
+      if (data?.errors) {
+        toast.error(`${data.errors[0].msg}`, {
+          theme: 'dark'
+        })
+      } else {
+        toast.success(`Usuario ${data.user} creado`, {
+          theme: 'dark'
+        })
+      }
+
     } catch (error) {
+      toast.error('No se puede registrar el usuario', {
+        theme: 'dark'
+      })
       console.log(error)
     }
   }
@@ -34,38 +47,49 @@ const FormRegister = () => {
         <h1 className='text-center p-3 register-title'>Registro</h1>
         <div className=' d-flex justify-content-center'>
           <form className='d-flex flex-column  register-form' onSubmit={handleSubmit(onSubmit)}>
+            <label for='nombre'>Nombre</label>
             <input
-              placeholder='Nombre'
+              placeholder='Pedro Picapiedras'
               maxLength={30}
-              type='text'{...register('name', { required: true, minLength: 3, maxLength: 12 })}
+              type='text'{...register('name', { required: true, minLength: 3, maxLength: 30 })}
             />
-            {errors.name && <span>Longitud mínima es 3 caracteres</span>}
+            {errors.name?.type === 'required' && <span>Campo requerido</span>}
+            {errors.name?.type === 'minLength' && <span>Longitud mínima es 3 caracteres</span>}
+            <label for='email'>Email</label>
             <input
-              placeholder='Email'
+              placeholder='email@email.com'
+              maxLength={40}
+              type='email' {...register('email', { required: true, pattern: /\S+@\S+\.\S+/ })}
+            />
+            {errors.email?.type === 'required' && <span>Campo requerido</span>}
+            {errors.email?.type === 'pattern' && <span>El formato de mail no es válido</span>}
+            <label for='adress'>Dirección</label>
+            <input
+              placeholder='Mi dirección 1234'
               maxLength={30}
-              type='email' {...register('email', { required: true, })}
+              type='text' {...register('adress', { required: true, minLength: 5 })}
             />
-            {errors.exampleRequired && <span>This field is required</span>}
+            {errors.adress?.type === 'required' && <span>Campo requerido</span>}
+            {errors.adress?.type === 'minLength' && <span>Longitud mínima de 5 caracteres</span>}
+            <label for='phoneNumber'>Número de teléfono</label>
             <input
-              placeholder='Dirección'
-              maxLength={30}
-              type='text' {...register('adress', { required: true, })}
+              placeholder='123456789'
+              maxLength={18}
+              type='text' {...register('phoneNumber', { required: true, minLength: 7, pattern: /^[0-9]+$/ })}
             />
-            {errors.exampleRequired && <span>This field is required</span>}
+            {errors.phoneNumber?.type === 'required' && <span>Campo requerido</span>}
+            {errors.phoneNumber?.type === 'minLength' && <span>Longitud mínima de 7 caracteres</span>}
+            {errors.phoneNumber?.type === 'pattern' && <span>Solo números</span>}
+            <label for='password'>Contraseña</label>
             <input
-              placeholder='teléfono'
               maxLength={12}
-              type='textsfd' {...register('phoneNumber', { required: true, })}
+              placeholder='Muysegura1234
+              '
+              type='password' {...register('password', { required: true, minLength: 8 })}
             />
-            {errors.exampleRequired && <span>This field is required</span>}
-            <input
-              maxLength={12}
-              placeholder='Password'
-              type='text' {...register('password', { required: true })}
-            />
-            {errors.password && <span>This field is required</span>}
-            <input type='submit' value='Registrarse' />
-            <input type='reset' value='Reset' />
+            {errors.password?.type === 'required' && <span>Campo requerido</span>}
+            {errors.password?.type === 'minLength' && <span>Longitus mínima de 8 caracteres</span>}
+            <input className='mt-5 register-form-submit' type='submit' value='Registrarse' />
           </form>
         </div>
         <div className='d-flex justify-content-center p-5'>
