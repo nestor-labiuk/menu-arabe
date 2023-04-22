3f1ff6854cfc944b47169bead10344d3a9d0b2f2
gabrielamansilla <gmansilla.utn.frt@gmail.com>
2023-04-22 19:33:49 -0300

    agrega ruta de login a componentes rutas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Register from './pages/Register/Register'
 import Home from './pages/Home/Home'
 import Admin from './pages/Admin/Admin'
+import Login from './pages/Login/login'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/admin' element={<Admin />} />
         {/* aca hay que agregar las rutas que faltan */}
+        <Route path='/login' element={<Login />} />
       </Routes>
     </>
   );
