8f19ad13e84873db2243e897a860f303a61390a0
gabrielamansilla <gmansilla.utn.frt@gmail.com>
2023-04-23 18:56:43 -0300

    cambio de estilos a pagina de logueo

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,63 +1,60 @@
-import React, { useState } from 'react'
+
 import { useForm } from 'react-hook-form';
+import Button from '../../components/Button/Button'
+import { Link } from 'react-router-dom'
 
 import './login.css';
 
-function Login() {
+const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
 
-  const[logged,setLogged] = useState(true)
-  const { register, handleSubmit, formState: {errors} } = useForm({ defaultValues: { email: "", password: ""} });
+  const createUsers = async (body) => {
+    try {
+      const response = await fetch('http://localhost:8080/api/users', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  const onSubmit = async (data) => {
-     fetch(``,{
-      method: 'POST',
-      body: JSON.stringify({
-        username: data.username,
-        email: data.email,
-        password: data.password
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then(res => {return res.json()})
-      .then(json => {
-        if(json.token){
-          localStorage.setItem('token', json.token)
-          localStorage.setItem('user', JSON.stringify(json.userdata))
-          setLogged(true)
-          window.location.replace('')
-        }else setLogged(false)
-      })  
+  const onSubmit = body => {
+    createUsers(body)
   }
 
   return (
-    <div>
-      <form className="container " onSubmit={handleSubmit(onSubmit)}>
-      <h1 className='login__title'>Login</h1>
-        <div className="mx-auto pt-2 form-group">
-          <h5 className="ps-2">Correo electrónico</h5>
-          <input type="email" className="form-control" placeholder="example@example.com" name="email" {...register("email", { required: true })} />
-          {errors?.email?.type === "required" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Este campo es obligatorio</span>}
-        </div>
-        <div className="mx-auto pt-2 form-group">
-          <h5 className="ps-2">Contraseña</h5>
-          <input type="password" className="form-control" placeholder="Contraseña" name="password" {...register("password", {required: true})} />
-          {errors?.password?.type === "required" && <span ><i className="bi bi-exclamation-octagon-fill me-2"></i>Este campo es obligatorio</span>}
-        </div>
-        {logged?  true : (
-          <div className="form-group fs-5 d-flex justify-content-center">
-            <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Correo o contraseña inválidos</span>
-          </div>
-        )}
-        
-        <div className="mx-auto pt-2 form-group d-flex my-2 justify-content-end">
-          <input type="submit" className="mt-2 btn btn-outline-pink-login" value="Iniciar sesión" />
-        </div>
-       
-      </form>
-    </div>
+      <div>
+      <form className="login__container " onSubmit={handleSubmit(onSubmit)}>
+      <h1 className='login__title'>Logueo</h1>
     
+      <form className='register__form' onSubmit={handleSubmit(onSubmit)}>
+        <input
+          maxLength={30}
+          placeholder='Email'
+          type='email' {...register('email', { required: true, })}
+        />
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input
+          maxLength={30}
+          type='text'
+          placeholder='Password' {...register('password', { required: true })}
+        />
+        {errors.password && <span>This field is required</span>}
+      </form>
+        <div className="mx-auto pt-2 form-group d-flex my-4 justify-content-end">
+        <input type="submit" className="mt-2 btn btn-outline-pink-login" value="Iniciar sesión" />
+        </div>
+       <div className='d-flex justify-content-around main-buttons'>
+        <Link to='/'><Button name='Volver' /></Link>
+      </div>
+      </form>
+      
+    </div>
   )
 }
 
