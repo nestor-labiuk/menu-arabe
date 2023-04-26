6b30f921640e9e08bd8a7929c2f3814d65790939
GabrielaMansilla <gmansilla.utn.frt@gmail.com>
2023-04-26 10:56:24 -0300

    modifico archivo login.js

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -10,7 +10,7 @@ const Login = () => {
 
   const createUsers = async (body) => {
     try {
-      const response = await fetch('http://localhost:8080/api/users', {
+      const response = await fetch('', {
         method: 'POST',
         body: JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' }
