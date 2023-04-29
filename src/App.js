0abb443a30b1cac788f7a1da98175573b0458476
Federico Carrizo <federicoleandrocarrizo12@gmail.com>
2023-04-29 17:34:11 -0300

    Navbar-Footer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css'
 import Register from './pages/Register/Register'
 import Home from './pages/Home/Home'
 import Admin from './pages/Admin/Admin'
+import Nav from './components/Navbar/Navbar'
+import Foot from './components/Footer/Footer'
 
 function App() {
+ 
   return (
     <>
+        <Nav/>  
       <Routes>
         <Route exact path='/' element={ <Home /> } />
         <Route path='/register' element={<Register />} />
         <Route path='/admin' element={<Admin />} />
         {/* aca hay que agregar las rutas que faltan */}
       </Routes>
+       <Foot/>
     </>
   );
 }
