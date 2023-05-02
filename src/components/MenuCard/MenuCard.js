b05f6ffd95f5e0dd053c3e46b1e9366d3bfb70da
AdrianMilo <adrianestebanlescano@gmail.com>
2023-05-02 20:27:06 -0300

    boton de crear menu del front

--- src/components/MenuCard/MenuCard.js
+++ src/components/MenuCard/MenuCard.js
@@ -1,15 +1,49 @@
 import React from 'react'
 import './menucard.css'
-import { Link } from 'react-router-dom'
+import Button from '../Button/Button'
+import { ToastContainer, toast } from 'react-toastify'
 
 const MenuCard = ({
-  name, id, category, detail, image, price 
+  name, id, category, detail, image, price, user, menu,
 }) => {
+  
+  const makeOrder = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/orders', {
+        method: 'POST',
+        body:JSON.stringify({ 
+          userName: user.user.name,
+          userAddress: user.user.adress,
+          menuName: name,
+          menuPrice: price            
+        }),
+          headers: { 'Content-Type': 'application/json' }
+        })
+        const data = await response.json()
+        console.log(data)
+  
+        if (data?.errors) {
+          toast.error(`${data.errors[0].msg}`, {
+            theme: 'dark'
+          })
+        } else {
+          toast.success('Pedido creado', {
+            theme: 'dark'
+          })
+        }
+  
+      } catch (error) {
+        toast.error('No se puede realizar el pedido. Se requiere iniciar sesión', {
+          theme: 'dark'
+        })
+        console.log(error)
+      }
+    }
 
   return (
     <main className='menumain'>
 
-      <article className='menu-card gap-3 px-3' id={id}>
+      <article className='menu-card gap-3 px-3' id={id} >
 
         <section className='card-body d-flex flex-column gap-2'>
           <hr/>
@@ -23,9 +57,10 @@ const MenuCard = ({
 
         <footer className='card-footer pb-2'>
           <h5>${Number(price).toFixed(2)}</h5>
-          <Link to='/order' className='link-menuCard'>Pedir Menú</Link>
+          <Button name='Pedir menú' onClick={makeOrder} />
         </footer>
       </article>
+      <div><ToastContainer/></div>
 
     </main>
   )
@@ -33,4 +68,3 @@ const MenuCard = ({
 
 export default MenuCard
 
-//<Link to={`/product/}${id}`}>
