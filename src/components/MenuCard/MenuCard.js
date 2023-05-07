af3f7f5f7a554c5aaa909725143163398defc1c2
AdrianMilo <adrianestebanlescano@gmail.com>
2023-05-07 19:30:16 -0300

    creación modal pedir menu

--- src/components/MenuCard/MenuCard.js
+++ src/components/MenuCard/MenuCard.js
@@ -52,6 +52,24 @@ const MenuCard = ({
         </footer>
       </article>
       <div><ToastContainer/></div>
+
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5 text-dark" id="exampleModalLabel"></h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body text-dark">
+              <p></p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"></button>
+              <button type="button" className="btn btn-primary"  data-bs-dismiss="modal" onClick={makeOrder} ></button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
