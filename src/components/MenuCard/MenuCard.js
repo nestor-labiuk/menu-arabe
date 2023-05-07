99e9594ff6cf95bfb4112db01ae766cd356bcf9e
AdrianMilo <adrianestebanlescano@gmail.com>
2023-05-07 19:38:19 -0300

    botón funcional pedir menú y modal creado

--- src/components/MenuCard/MenuCard.js
+++ src/components/MenuCard/MenuCard.js
@@ -47,23 +47,23 @@ const MenuCard = ({
         </section>
         <footer className='card-footer pb-2 mt-2'>
           <h5>${Number(price).toFixed(2)}</h5>
-          <button className='m-2 buttonMenu' data-bs-toggle="modal" data-bs-target="#menuModal">Pedir Menú</button>
+          <button className='m-2 buttonMenu' data-bs-toggle="modal" data-bs-target="#exampleModal">Pedir Menú</button>
         </footer>
       </article>
       <div><ToastContainer/></div>
-      <div className="modal fade" id="menuModal" tabindex="-1" aria-labelledby="menuModalLabel" aria-hidden="true">
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5 text-dark" id="menuModalLabel">Confirmación</h1>
+              <h1 className="modal-title fs-5 text-dark" id="exampleModalLabel">Confirmación</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body text-dark">
-              <p>¿Confirma el pedido?</p>
+              <p>¿Está seguro de que confirma el pedido?</p>
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-              <button type="button" className="btn btn-primary"  data-bs-dismiss="modal" onClick={makeOrder}>Confirmar</button>
+              <button type="button" className="btn btn-primary"  data-bs-dismiss="modal" onClick={makeOrder} >Confirmar</button>
             </div>
           </div>
         </div>
