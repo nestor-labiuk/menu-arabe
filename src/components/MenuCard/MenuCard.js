585161f8ebd490897bda6855f51b847c69800b1f
AdrianMilo <adrianestebanlescano@gmail.com>
2023-05-08 19:47:50 -0300

    Corrección de estilos de las card

--- src/components/MenuCard/MenuCard.js
+++ src/components/MenuCard/MenuCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './menucard.css'
-import Button from '../Button/Button'
 import { ToastContainer, toast } from 'react-toastify'
 
 const MenuCard = ({
@@ -36,22 +35,38 @@ const MenuCard = ({
     }
   return (
     <main className='menumain'>
-      <article className='menu-card gap-3 px-3' id={id} >
-        <section className=' card-body d-flex flex-column gap-2'>
-          <hr/>
-          <h5>{name}</h5>
-          <img src={image} alt={name}/>
+      <article className='menu-card text-center mt-3' id={id} >
+          <h5 className='d-flex justify-content-center mt-3 title-card'>{name}</h5>
+        <section className='card-body d-flex flex-column gap-2'>
+          <img src={image} alt={name} className=''/>
           <div>
             <span className='badge text-bg-secondary'>{category}</span>
           </div>
           <p>{detail}</p>
         </section>
-        <footer className='card-footer pb-2 mt-2'>
+        <footer className='card-footer pb-2 mb-2'>
           <h5>${Number(price).toFixed(2)}</h5>
-          <Button name='Pedir menú' onClick={makeOrder} />
+          <button className='m-2 buttonMenu' data-bs-toggle="modal" data-bs-target="#exampleModal">Pedir Menú</button>
         </footer>
       </article>
       <div><ToastContainer/></div>
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5 text-dark" id="exampleModalLabel">Confirmación</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body text-dark">
+              <p>¿Está seguro de que confirma el pedido?</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" className="btn btn-primary"  data-bs-dismiss="modal" onClick={makeOrder} >Confirmar</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
